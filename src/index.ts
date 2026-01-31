40a439eaf80250656c8c9c7821042f6acaba82b2
Gustavo Carbonera <guga_carbo@hotmail.com>
2026-01-31 12:53:43 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-//
-//
+import z from "../node_modules/zod/index.cjs";
+
+const userSchema = z.object({
+	id: z.number().int().positive(),
+	name: z.string().min(2).max(100),
+	email: z.email(),
+	createdAt: z.date(),
+});
+
+type User = z.infer<typeof userSchema>;
+
+export { userSchema };
+export type { User };
